b04dbbe08c4390a8f1615e805fb566c0b67724b8
Jay Schauer <lacrossejason@gmail.com>
2018-01-23 02:25:32 -0600

    fixed constructor

--- LearningRobot/src/org/usfirst/frc/team1732/robot/commands/DriveBackwardsTwice.java
+++ LearningRobot/src/org/usfirst/frc/team1732/robot/commands/DriveBackwardsTwice.java
@@ -1,11 +1,15 @@
 package org.usfirst.frc.team1732.robot.commands;
 
+import org.usfirst.frc.team1732.robot.Robot;
+
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class DriveBackwardsTwice extends CommandGroup {
 
 	public DriveBackwardsTwice() {
-		addSequential(new DriveBackwards());
-		addSequential(new DriveBackwards());
+		addSequential(new DriveBackwards(Robot.drivetrain.leftEncoder.makeReader(),
+				Robot.drivetrain.rightEncoder.makeReader()));
+		addSequential(new DriveBackwards(Robot.drivetrain.leftEncoder.makeReader(),
+				Robot.drivetrain.rightEncoder.makeReader()));
 	}
 }
